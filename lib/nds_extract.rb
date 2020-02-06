7d1393bc3f0e84bd6f82f57172eb481e4841d368
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-06 06:44:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -109,7 +109,6 @@ def movies_with_directors_set(source)
   # RETURN:
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-<<<<<<< HEAD
   #binding.pry
   a =[]
   i=0
